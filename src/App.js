96fbed150495122ead0ead5090b3d8de239965ab
unknown <alinashrestha723@gmail.com>
2023-03-19 15:59:34 +0545

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Navbar from "./Components/Navbar"
+import {Route} from "react-router-dom"
+
+function App() {
+  return (
+    
+      <div className="App">
+        <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react"
+import {Component} from "react"
+import "./Navbar.css"
+import {Link} from "react-router-dom"
+import { MenuItems } from "./MenuItems"
+
+
+class Navbar extends Component {
+
+    state = {clicked:false};
+    handleClick =() =>{
+        this.setState({clicked:!this.state.clicked})
+    }
+    render() {
+        return(
+            <nav className ="navbar-items">
+                <h1 className="navbar-logo">Alina</h1>
+
+                <div className="menu-icons" onClick={this.handleClick}>
+                <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+
+                <ul className={this.state.clicked ? "nav-menu active":"nav-menu"}>
+                    {MenuItems.map((item, index) => {
+                        return(
+                            <li key={{index}}>
+                            <a className ={item.cName} href ="/">
+                            <i className={item.icon}> </i> 
+                            {item.title}
+                            
+                            </a>
+                        </li>
+                        )
+                    }
+                    )}
+                   
+                   <button>Sign Up</button>
+                </ul>
+
+
+            </nav>
+        )
+    }
+}
+
+export default Navbar
