89bfa11de3062825264b1d8c3b9509d25c07f610
unknown <alinashrestha723@gmail.com>
2023-03-19 17:40:13 +0545

    hero . js

---
+++ src/Components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <>
+    <div className = "hero">
+      <img alt="Heroimage" src="https://images.unsplash.com/photo-1679215805580-8f5946098bf1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60"/>
+    </div>
+    </>
+    )
+}
+
+export default Hero
---
+++ src/Routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../Components/Navbar"
+import Hero from "../Components/Hero"
+const Home = () => {
+  return (
+    <>
+        <h1> HOME</h1>
+        <Navbar/>
+            <Hero/>
+    </>
+  )
+}
+
+export default Home
